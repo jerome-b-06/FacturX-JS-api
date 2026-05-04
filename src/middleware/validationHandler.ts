779c06effa8f94f74271ac809a07b5f81a42dc59
jerome <jerome.bonelli@gmail.com>
2026-05-04 12:05:50 +0200

    modify error handling to catch zod errors

--- src/middleware/validationHandler.ts
+++ src/middleware/validationHandler.ts
@@ -3,7 +3,11 @@ import type {Request, Response, NextFunction} from "express";
 
 export function validationHandler(schema: z.ZodObject<any, any>) {
     return (req: Request, res: Response, next: NextFunction) => {
+        try {
             schema.parse(req.body);
             next();
+        } catch (error) {
+            next(error); // Passe au errorHandler
+        }
     };
 }
